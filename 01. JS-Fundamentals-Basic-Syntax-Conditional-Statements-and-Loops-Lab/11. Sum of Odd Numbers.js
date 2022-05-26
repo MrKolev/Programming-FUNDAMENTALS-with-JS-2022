0e5bb5ad60c706c9f0c3fe430cba379f0fbb5af8
MrKolev <100051403+MrKolev@users.noreply.github.com>
2022-05-26 17:51:05 +0100

    solution of problem 11

--- 01. JS-Fundamentals-Basic-Syntax-Conditional-Statements-and-Loops-Lab/11. Sum of Odd Numbers.js
+++ 01. JS-Fundamentals-Basic-Syntax-Conditional-Statements-and-Loops-Lab/11. Sum of Odd Numbers.js
@@ -1,4 +1,16 @@
-function sumOfOddNumbers(){
+function sumOfOddNumbers(num) {
+    let nextNumber = 1;
+    let sum = 0;
+    let counter = 0;
+    while (counter < num) {
+        if (nextNumber % 2 !== 0) {
+            console.log(nextNumber)
+            counter++;
+            sum += nextNumber;
+        }
+        nextNumber += 1;
+    }
+console.log(`Sum: ${sum}`);
 
-    
 }
+sumOfOddNumbers(5)
